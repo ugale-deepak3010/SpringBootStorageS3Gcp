486270a21217e95550fd0670a1861dc0db5c2800
Dipak Ugale <dipak.ugale@corrigo.com>
2024-09-05 09:34:53 +0530

    Initilize

---
+++ src/main/java/com/SpringBootStorageS3Gcp/SpringBootStorageS3GcpApplication.java
@@ -0,0 +1,13 @@
+package com.SpringBootStorageS3Gcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStorageS3GcpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootStorageS3GcpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SpringBootStorageS3Gcp/SpringBootStorageS3GcpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringBootStorageS3Gcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStorageS3GcpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
